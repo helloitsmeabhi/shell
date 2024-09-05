8c82a6a9d9071058090329b80cb1c6b24b742f5d
helloitsmeabhi <helloitsmeabhinav2003@gmail.com>
2024-09-05 14:58:27 +0530

    arrays stuffs

--- arrays/arr1.go
+++ arrays/arr1.go
@@ -3,12 +3,67 @@ package main
 import "fmt"
 
 func main() {
-	arraydeclare()
+	//array1()
+	//sumavg()
+	//usingrange()
+	//usingshort()
+	checkingvaluesofrange()
 }
-func arraydeclare() {
+func array1() {
 	var x [5]int
-	for i := 0; i < 5; i++ {
-		x[i] = i
-		fmt.Println(x[i])
+	x[4] = 100
+	fmt.Println(x)
+	fmt.Println(x[4])
+}
+func sumavg() {
+	var x [5]int
+	x[0] = 100
+	x[1] = 200
+	x[2] = 300
+	x[3] = 400
+	x[4] = 500
+	var total float64
+	var avg float64
+	for i := 0; i < len(x); i++ {
+		total += float64(x[i])
 	}
+	avg = total / float64(len(x))
+	fmt.Print(x)
+	fmt.Println()
+	fmt.Print(total)
+	fmt.Println()
+	fmt.Print(avg)
+}
+func usingrange() {
+	var total float64 = 0
+	var x [5]int
+	x[0] = 100
+	x[1] = 200
+	x[2] = 300
+	x[3] = 400
+	x[4] = 500
+	for _, value := range x {
+		total += float64(value)
+	}
+	fmt.Println(total / float64(len(x)))
+
+}
+func usingshort() {
+	var total float64
+	x := [5]float64{200, 300, 400, 500, 600}
+	for _, value := range x {
+		total += value
+	}
+	fmt.Println(total)
+}
+func checkingvaluesofrange() {
+	var total float64
+	x := [5]float64{300, 400, 300, 600, 500}
+	for i, value := range x {
+		fmt.Print(i)
+		fmt.Print(" ")
+		fmt.Println(value)
+		total += value
+	}
+	fmt.Println(total)
 }
