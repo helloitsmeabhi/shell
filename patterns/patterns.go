42725e9bc50bddb731d6cb0902de862fb942ad4e
helloitsmeabhi <helloitsmeabhinav2003@gmail.com>
2024-09-05 12:27:56 +0530

    new patterns:

--- patterns/patterns.go
+++ patterns/patterns.go
@@ -3,14 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	sine()
+	//matrix()
+	construct()
 }
 func test() {
 	for i := 1; i < 10; i++ {
 		fmt.Println("*")
 	}
 }
-func sine() {
+func matrix() {
 	var n int
 	fmt.Println("Enter a number")
 	fmt.Scanf("%d", &n)
@@ -51,6 +52,17 @@ func sine() {
 		for j := 1; j <= i; j++ {
 			fmt.Print(j)
 			fmt.Print(" ")
+		}
+		fmt.Println()
+	}
+}
+func construct() {
+	fmt.Print("Enter: ")
+	var n int
+	fmt.Scanf("%d", &n)
+	for i := n; i >= 1; i-- {
+		for j := 1; j <= i; j++ {
+			fmt.Print("* ")
 		}
 		fmt.Println()
 	}
